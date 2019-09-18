12c3564f62dceddf20a1cfcf5efa9700c32b5cbd
John Shewchuk <johnshew@users.noreply.github.com>
2019-09-18 11:27:31 -0700

    Initial Azure AD implementation

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 This example demonstrates how to use [Express](http://expressjs.com/) 4.x and
-[Passport](http://passportjs.org/) to authenticate users using a username and
-password with [form-based authentication](https://en.wikipedia.org/wiki/HTTP%2BHTML_form-based_authentication).
+[Passport](http://passportjs.org/) to authenticate users using Azure AD.
 Use this example as a starting point for your own web applications.
 
 ## Instructions
@@ -9,18 +8,17 @@ To install this example on your computer, clone the repository and install
 dependencies.
 
 ```bash
-$ git clone https://github.com/passport/express-4.x-local-example.git
-$ cd express-4.x-local-example
+$ git clone https://github.com/johnshew/passport-express-4.x-azure-ad.git
+$ cd passport-express-4.x-azure-ad
 $ npm install
+$ npm build
 ```
 
 Start the server.
 
 ```bash
-$ node server.js
+$ node dist/server.js
 ```
 
 Open a web browser and navigate to [http://localhost:3000/](http://127.0.0.1:3000/)
-to see the example in action.  Log in using username `jack` and password `secret`.
-
-
+to see the example in action. 
---
+++ src/server.ts
@@ -0,0 +1,150 @@
+require('dotenv').config();
+
+import * as express from 'express';
+import * as passport from 'passport';
+import * as azure_ad from 'passport-azure-ad';
+import fetch from 'node-fetch';
+
+// tslint:disable-next-line: no-var-requires
+require('dotenv').config();
+
+const baseUrl = process.env.BASE_URL || 'http://localhost:3000/';
+const redirectPath = 'auth/openid/return';
+const port = process.env.PORT || '3000';
+
+const azureStrategyOptions: azure_ad.IOIDCStrategyOptionWithRequest = {
+  identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+  clientID: process.env.APP_ID,
+  clientSecret: process.env.APP_SECRET,
+  responseType: 'code id_token',
+  responseMode: 'form_post',
+  redirectUrl: baseUrl + redirectPath,
+  allowHttpForRedirectUrl: true, // in production make false
+  validateIssuer: false, // in production make true
+  isB2C: false,
+  issuer: null,
+  passReqToCallback: true,
+  scope: ['profile', 'offline_access', 'https://graph.microsoft.com/user.readwrite'], // remove offline_access if you only want the app to be functional during login
+  loggingLevel: 'info',
+  nonceLifetime: null, // defaults to 3600 seconds
+  nonceMaxAmount: 10, // 10 is default
+  useCookieInsteadOfSession: false,
+  cookieEncryptionKeys: null, // not required if useCoookieInsteadOfSession is false
+  clockSkew: null, // 
+};
+
+async function processAzureStrategy(
+  req: express.Request,
+  iss: string,
+  sub: string,
+  profile: azure_ad.IProfile,
+  jwtClaims: any,
+  access_token: string,
+  refresh_token: string,
+  oauthToken: any,
+  done: azure_ad.VerifyCallback) {
+
+  if (!profile.oid) {
+    return done(new Error('No oid found'), null);
+  }
+  process.nextTick(async () => {
+    return done(null, { ...profile, oauthToken }); // Done and include OauthToken in the profile.
+  });
+}
+
+// Augument Passport's request.user with the Azure AD oauthToken
+declare global {
+  namespace Express {
+    interface User {
+      oauthToken: any;
+      [key: string]: any;
+      
+    }
+  }
+}
+
+passport.use(new azure_ad.OIDCStrategy(azureStrategyOptions, processAzureStrategy));
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Azure AD profile is serialized
+// and deserialized.
+
+passport.serializeUser(function (user, cb) {
+  console.log(user)
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  console.log(obj);
+  cb(null, obj);
+});
+
+// Create a new Express application.
+var app = express();
+
+// Configure view engine to render EJS templates.
+app.set('views', require('path').join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('cookie-parser')()); // required by Azure-AD return parsing
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/',
+  function (req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function (req, res) {
+    res.render('login');
+  });
+
+app.get('/login/azure-ad',
+  passport.authenticate('azuread-openidconnect'));
+
+
+app.get('/' + redirectPath,
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+app.post('/' + redirectPath,
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout',
+  function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  async (req, res, next) => {
+    const response = await fetch('https://graph.microsoft.com/v1.0/me', { headers: { 'Authorization': `Bearer ${req.user.oauthToken.access_token}` } });
+    if (!response.ok) return next(response.statusText);
+    let profile = await response.json();
+    res.render('profile', { user: profile });
+    return next();
+  });
+
+app.listen(process.env['PORT'] || 3000);
