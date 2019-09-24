81d7e08be7c8fb4a78fc7ba811ef2081ad265ac5
John Shewchuk <johnshew@users.noreply.github.com>
2019-09-24 12:45:28 -0700

    Update template

--- src/server.ts
+++ src/server.ts
@@ -120,7 +120,7 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('cookie-parser')()); // required by Azure-AD return parsing
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require('express-session')({ secret: process.env.COOKIE_SECRET, resave: false, saveUninitialized: false }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
